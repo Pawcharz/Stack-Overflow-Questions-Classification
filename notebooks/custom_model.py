d8079f1b4f4e98438255d149ae36711d5b0f9b87
Pawcharz <pawel030724@gmail.com>
2024-01-01 20:20:33 +0100

    Visibly remove nan loss error for transformer architecture

--- notebooks/custom_model.py
+++ notebooks/custom_model.py
@@ -11,27 +11,12 @@ class AutoCompositeModel(nn.Module):
     # self.model_content = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=64)
     # self.model_title = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=32)
     
-    # self.transformerEncoder = nn.TransformerEncoderLayer(d_model=128, nhead=8)
     # self.lstm = nn.LSTM(128, 128, 3)
-    # self.test_model = nn.Sequential(
-    #   nn.LayerNorm(128),
-    #   nn.Linear(128, 256),
-    #   nn.ReLU(),
-    #   nn.Linear(256, 64),
-    #   nn.ReLU(),
-    #   nn.Linear(64, 256),
-    #   nn.ReLU(),
-    #   nn.Linear(256, 128),
-    #   nn.ReLU(),
-    #   nn.Linear(128, 5)
-    # )
-  
-    self.lstm = nn.LSTM(128, 256, 3)
+    
+    self.transformerEncoder = nn.TransformerEncoderLayer(d_model=128, nhead=8, norm_first=True)
     self.test_model = nn.Sequential(
-      nn.LayerNorm(256),
-      nn.Linear(256, 512),
-      nn.ReLU(),
-      nn.Linear(512, 256),
+      nn.LayerNorm(128),
+      nn.Linear(128, 256),
       nn.ReLU(),
       nn.Linear(256, 64),
       nn.ReLU(),
@@ -41,13 +26,29 @@ class AutoCompositeModel(nn.Module):
       nn.ReLU(),
       nn.Linear(128, 5)
     )
+  
+    # self.lstm = nn.LSTM(128, 256, 3)
+    # self.test_model = nn.Sequential(
+    #   nn.LayerNorm(256),
+    #   nn.Linear(256, 512),
+    #   nn.ReLU(),
+    #   nn.Linear(512, 256),
+    #   nn.ReLU(),
+    #   nn.Linear(256, 64),
+    #   nn.ReLU(),
+    #   nn.Linear(64, 256),
+    #   nn.ReLU(),
+    #   nn.Linear(256, 128),
+    #   nn.ReLU(),
+    #   nn.Linear(128, 5)
+    # )
     # self.my_new_layers = nn.Sequential(
     #   nn.Linear(128, 5),
     # )
   def forward(self, x):
 
-    x = self.lstm(x)[0]
-    # x = self.transformerEncoder(x)
+    # x = self.lstm(x)[0]
+    x = self.transformerEncoder(x)
     
     x = self.test_model(x)
     
