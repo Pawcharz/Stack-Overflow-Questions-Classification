3ede158fceb13199428409b17120c5ec4e1288bd
Pawcharz <pawel030724@gmail.com>
2024-01-01 19:54:57 +0100

    Train larger LSTM model for 8 epochs

--- notebooks/custom_model.py
+++ notebooks/custom_model.py
@@ -11,35 +11,43 @@ class AutoCompositeModel(nn.Module):
     # self.model_content = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=64)
     # self.model_title = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=32)
     
-    # encoder_layer = nn.TransformerEncoderLayer(d_model=128, nhead=8)
-    # self.model_content = encoder_layer # nn.TransformerEncoder(encoder_layer, num_layers=6)
-    # self.my_new_layers = nn.Sequential(
+    # self.transformerEncoder = nn.TransformerEncoderLayer(d_model=128, nhead=8)
+    # self.lstm = nn.LSTM(128, 128, 3)
+    # self.test_model = nn.Sequential(
     #   nn.LayerNorm(128),
-    #   nn.Linear(128, 24),
+    #   nn.Linear(128, 256),
     #   nn.ReLU(),
-    #   nn.Linear(24, 128),
+    #   nn.Linear(256, 64),
+    #   nn.ReLU(),
+    #   nn.Linear(64, 256),
+    #   nn.ReLU(),
+    #   nn.Linear(256, 128),
     #   nn.ReLU(),
     #   nn.Linear(128, 5)
     # )
-    self.lstm = nn.LSTM(128, 128, 2)
+  
+    self.lstm = nn.LSTM(128, 256, 3)
     self.test_model = nn.Sequential(
-      # nn.LayerNorm(128),
-      # # nn.TransformerEncoderLayer(d_model=128, nhead=8),
-      # nn.LSTM(128, 32, 2),
-      nn.LayerNorm(128),
-      nn.Linear(128, 32),
+      nn.LayerNorm(256),
+      nn.Linear(256, 512),
       nn.ReLU(),
-      nn.Linear(32, 128),
+      nn.Linear(512, 256),
+      nn.ReLU(),
+      nn.Linear(256, 64),
+      nn.ReLU(),
+      nn.Linear(64, 256),
+      nn.ReLU(),
+      nn.Linear(256, 128),
       nn.ReLU(),
       nn.Linear(128, 5)
     )
-  
     # self.my_new_layers = nn.Sequential(
     #   nn.Linear(128, 5),
     # )
   def forward(self, x):
 
     x = self.lstm(x)[0]
+    # x = self.transformerEncoder(x)
     
     x = self.test_model(x)
     
