e6af4824cbf954cf2313f024ac86f56af43a9e6a
Pawcharz <pawel030724@gmail.com>
2024-03-04 17:53:31 +0100

    Create separate models to further join

--- notebooks/custom_model.py
+++ notebooks/custom_model.py
@@ -3,64 +3,91 @@ import numpy as np
 from torch import nn
 from transformers import AutoModelForSequenceClassification
 
-class AutoCompositeModel2(nn.Module):
+# 'content_input_ids', 'title_input_ids', 'tags_onehot', 'unrecognized_tags_count', 'reputation', 'undeleted_answers', 'user_life_days'
+
+class TagsModel(nn.Module):
   def __init__(self, device):
-    super(AutoCompositeModel, self).__init__()
+    super(TagsModel, self).__init__()
     
     self.device = device
-    # self.model_content = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=64)
-    # self.model_title = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=32)
-    
-    # self.lstm = nn.LSTM(128, 128, 3)
-    
-    # input - 128
-    self.contentEncoder = nn.TransformerEncoderLayer(d_model=128, nhead=4, norm_first=True)
-    # input - 32
-    self.titleEncoder = nn.TransformerEncoderLayer(d_model=32, nhead=2, norm_first=True)
-    
-    # self.encoding_expander_title = nn.Sequential(
-    #   nn.Linear(32, 128),
-    #   nn.ReLU(),
-    #   nn.Linear(128, 256),
-    # )
     
     self.forward_layer = nn.Sequential(
-      nn.Linear(160, 256),
-      # nn.Linear(164, 256),
+      nn.Linear(100, 86),
       nn.ReLU(),
-      nn.Linear(256, 512),
-      nn.ReLU(),
-      nn.Linear(512, 512),
-      nn.ReLU(),
-      nn.Linear(512, 128),
-      nn.ReLU(),
-      nn.Linear(128, 256),
-      nn.ReLU(),
-      nn.Linear(256, 256),
-      nn.ReLU(),
-      nn.Linear(256, 128),
-      nn.ReLU(),
-      nn.Linear(128, 5)
+      # nn.Linear(86, 32),
+      # nn.ReLU(),
+      nn.Linear(86, 5)
     )
   def forward(self, inputs):
         
-    text_content = inputs['content_input_ids'].to(self.device).float()
-    title = inputs['title_input_ids'].to(self.device).float()
-    
-    # urecognized_tags = inputs['unrecognized_tags_count'].reshape(-1, 1).to(self.device).float()
-    # reputation = inputs['reputation'].reshape(-1, 1).to(self.device).float()
-    # undeleted = inputs['undeleted_answers'].reshape(-1, 1).to(self.device).float()
-    # life_days = inputs['user_life_days'].reshape(-1, 1).to(self.device).float()
-    
-    x_content = self.contentEncoder(text_content).float()
-    x_title = self.titleEncoder(title).float()
-    # rest = torch.cat((reputation, undeleted, life_days, urecognized_tags), 1)
-    
-    x_concated = torch.cat((x_content, x_title), 1)
-    x = self.forward_layer(x_concated)
+    tags_input = inputs['tags_onehot'].to(self.device).float()
+   
+    x = self.forward_layer(tags_input)
     
     return x
   
+# 'unrecognized_tags_count', 'reputation', 'undeleted_answers', 'user_life_days'
+class NumericalPartModel(nn.Module):
+  def __init__(self, device):
+    super(NumericalPartModel, self).__init__()
+    
+    self.device = device
+    
+    self.forward_layer = nn.Sequential(
+      nn.Linear(4, 128),
+      nn.ReLU(),
+      nn.Linear(128, 256),
+      nn.ReLU(),
+      nn.Linear(256, 128),
+      nn.ReLU(),
+      nn.Linear(128, 5),
+    )
+  def forward(self, inputs):
+        
+    unrec_tags_input = inputs['unrecognized_tags_count'].to(self.device).float()
+    reputation_input = inputs['reputation'].to(self.device).float()
+    answers_input = inputs['undeleted_answers'].to(self.device).float()
+    life_input = inputs['user_life_days'].to(self.device).float()
+   
+    all = torch.stack((unrec_tags_input, reputation_input, answers_input, life_input), 1)
+    
+    x = self.forward_layer(all)
+    
+    return x
+
+# 'content_input_ids', 'title_input_ids',
+class TextualPartModel(nn.Module):
+  def __init__(self, device):
+    super(TextualPartModel, self).__init__()
+    
+    self.device = device
+    
+    # input - 32
+    self.titleEncoder = nn.TransformerEncoderLayer(d_model=32, nhead=2, norm_first=True)
+    
+    # input - 128
+    self.contentEncoder = nn.TransformerEncoderLayer(d_model=128, nhead=4, norm_first=True)
+    
+    self.forward_layer = nn.Sequential(
+      nn.Linear(160, 86),
+      nn.ReLU(),
+      nn.Linear(86, 5),
+    )
+  def forward(self, inputs):
+        
+    title_input = inputs['title_input_ids'].to(self.device).float()
+    content_input = inputs['content_input_ids'].to(self.device).float()
+    
+    title_input = self.titleEncoder(title_input)
+    content_input = self.contentEncoder(content_input)
+   
+    all = torch.cat((title_input, content_input), 1)
+    
+    x = self.forward_layer(all)
+    
+    return x
+
+ 
 class AutoCompositeModel(nn.Module):
   def __init__(self, device):
     super(AutoCompositeModel, self).__init__()
@@ -68,22 +95,33 @@ class AutoCompositeModel(nn.Module):
     self.device = device
     
     self.forward_layer = nn.Sequential(
-      nn.Linear(4, 32),
+      nn.Linear(5, 32),
       nn.ReLU(),
       nn.Linear(32, 12),
       nn.ReLU(),
       nn.Linear(12, 5)
     )
+    
+    self.tags_layer = nn.Sequential(
+      nn.Linear(100, 48),
+      nn.Linear(48, 1),
+    )
+    
   def forward(self, inputs):
     
     # 'unrecognized_tags_count', 'reputation', 'tags_onehot', 'undeleted_answers', 'user_life_days'
+    
+    tags_onehot = inputs['tags_onehot'].reshape(-1, 1).to(self.device).float()
+    print(tags_onehot.size(), tags_onehot)
+    
+    tags_output = self.tags_layer(tags_onehot)
     
     urecognized_tags = inputs['unrecognized_tags_count'].reshape(-1, 1).to(self.device).float()
     reputation = inputs['reputation'].reshape(-1, 1).to(self.device).float()
     undeleted = inputs['undeleted_answers'].reshape(-1, 1).to(self.device).float()
     life_days = inputs['user_life_days'].reshape(-1, 1).to(self.device).float()
     
-    all = torch.cat((reputation, undeleted, life_days, urecognized_tags), 1)
+    all = torch.cat((reputation, undeleted, life_days, urecognized_tags, tags_output), 1)
   
     x = self.forward_layer(all)
     
